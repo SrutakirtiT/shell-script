3aa313ee678144591f99681131fbb5804280ca6d
sruthakeerthi <sruthakeerthi53@gmail.com>
2025-01-08 20:12:12 +0800

    shell-script

--- 13.logs.sh
+++ 13.logs.sh
@@ -5,7 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -fi)
+LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%h-%m-%S)
 LOG_FILE_NAME="$LOGS_FOLEDER/$LOG_FILE-$TIMESTAMP.log"
 
