5ab9ecab853b942dafe29b1a560f2898066b8f63
sruthakeerthi <sruthakeerthi53@gmail.com>
2025-01-08 19:31:02 +0800

    shell-script

--- 12-colors.sh
+++ 12-colors.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2...FAILURE"
+        echo  -e "$2... $R FAILURE"
         exit 1
     else
-        echo "$2...SUCCESS"
+        echo -e "$2...$G SUCCESS"
     fi
 }
 
@@ -25,7 +28,7 @@ then #not installed
     dnf install mysql -y
     VALIDATE $? "INSTALLING MySQL"
 else
-    echo "MySQL is already.....INSTALLLED"
+    echo -e "MySQL is already..... $Y INSTALLLED"
 fi
 
 
@@ -36,5 +39,5 @@ then
     dnf install git -y
     VALIDATE $? "INSTALLING Git"
 else
-    echo "Git is already....INSTALLED"
+    echo -e "Git is already.... $Y INSTALLED"
 fi
